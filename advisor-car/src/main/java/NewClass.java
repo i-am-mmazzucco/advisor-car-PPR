defa9af706a41d663c45b4be92d9781784ae0c86
mmazzucco <mateomazzucco4@gmail.com>
2023-11-07 19:57:56 -0300

    First commit

---
+++ advisor-car/src/main/java/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mmazzucco
+ */
+public class NewClass {
+    public static void main(String []args) {
+        System.out.println("My First Java Program.");
+    }
+}
