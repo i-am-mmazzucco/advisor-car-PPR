8db4f86c48ef92445410afdf258bd5ee79859baa
Juan Cruz Mare <jcmare18@gmail.com>
2023-11-08 21:00:26 -0300

    Adding posible classes

---
+++ src/main/java/vehiculo/Automovil.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vehiculo;
+
+/**
+ *
+ * @author juansa6201
+ */
+public class Automovil implements Vehiculo {
+
+    @Override
+    public void acelerar() {
+        System.out.println("Acelerar el auto");
+    }
+    
+}
